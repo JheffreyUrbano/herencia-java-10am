848b08c6063f5667c93057cd33a176a72b62064f
JheffreyUrbano <jheffrey2002@gmail.com>
2026-09-23 11:22:56 -0500

    Creación de la clase principal, se une el Controlador, el Modelo y la Vista.

--- src/main/java/com/mycompany/simuladordnd/SimuladorDND.java
+++ src/main/java/com/mycompany/simuladordnd/SimuladorDND.java
@@ -1,16 +1,33 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.simuladordnd;
 
 /**
  *
  * @author Jurbano
  */
+import Modelo.*;
+import Vista.VistaCombate;
+import Controlador.ControladorCombate;
+
 public class SimuladorDND {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Simulador DND");
+
+        // Inicializar el modelo.
+        Personaje explorador = new Ranger("simon", 15);
+        Personaje guerrero = new Paladin("tatiana", 28);
+        Personaje Profe = new Personaje("edwin", 10);
+
+        Personaje[] miGrupo = {explorador, guerrero, Profe};
+
+        VistaCombate miVista = new VistaCombate();
+
+        // Inicializamos el controlador
+        ControladorCombate control = new ControladorCombate(miGrupo, miVista);
+        control.ejecutarRonda();
+
     }
 }
